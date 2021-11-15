278ab0fcb64002feb64e1f168a0cc3356918b75a
Md Sabbir Hossain <m.sabbirhossain308@gmail.com>
2021-11-16 00:20:26 +0600

    dsdsfs

--- pages/urls.py
+++ pages/urls.py
@@ -4,5 +4,9 @@ from . import views
 
 urlpatterns = [
     path('',views.home, name='home'),
-    
+    # path('cars',views.cars, name='cars'),
+    path('about',views.about, name='about'),
+    path('services',views.services, name='services'),
+    path('contact',views.contact, name='contact'),
+
 ]
