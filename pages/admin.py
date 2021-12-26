eeb5bea3256a596fb198d229ace1cdf691855eea
Md Sabbir Hossain <m.sabbirhossain308@gmail.com>
2021-12-26 21:59:58 +0600

    teams model

--- pages/admin.py
+++ pages/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 from .models import Teams
+from django.utils.html import format_html
 
 # Register your models here.
+class TeamAdmin(admin.ModelAdmin):
+    def thumbnail(self, object):
+        return format_html('<img src="{}" width="50" style="border-radius:50px;" />'.format(object.photo.url))
 
-admin.site.register(Teams)
+    thumbnail.short_description = 'Image'
+
+    list_display = ('id', 'thumbnail' , 'first_name', 'last_name', 'created_date')
+    list_display_links = ('id', 'thumbnail', 'first_name',)
+    search_fields = ('first_name', 'last_name', 'designation')
+    list_filter = ('designation',)
+
+
+
+admin.site.register(Teams, TeamAdmin)
