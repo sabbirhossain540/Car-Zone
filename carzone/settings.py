15177cd6dd038cba89bc7ef50410e2e1a549436c
Md Sabbir Hossain <m.sabbirhossain308@gmail.com>
2021-12-26 20:59:18 +0600

    database connection

--- carzone/settings.py
+++ carzone/settings.py
@@ -76,9 +76,16 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'carzone',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost'
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
