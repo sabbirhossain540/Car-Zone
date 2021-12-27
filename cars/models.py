c221572b21ccae4935e1c7fc6a02e074ebd2d343
Md Sabbir Hossain <m.sabbirhossain308@gmail.com>
2021-12-27 20:49:14 +0600

    dsd

--- cars/models.py
+++ cars/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from datetime import datetime
-
+from ckeditor.fields import RichTextField
+from multiselectfield import MultiSelectField
 # Create your models here.
 class Car(models.Model):
     state_choice = (
@@ -96,13 +97,13 @@ class Car(models.Model):
     year = models.IntegerField(('year'), choices=year_choice)
     condition = models.CharField(max_length=100)
     price = models.IntegerField()
-    description = models.TextField(max_length=500)
+    description = RichTextField()
     car_photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
     car_photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     car_photo_2 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     car_photo_3 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     car_photo_4 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
-    features = models.CharField(choices=features_choices, max_length=255)
+    features = MultiSelectField(choices=features_choices)
     body_style = models.CharField(max_length=255)
     engine = models.CharField(max_length=255)
     transmission = models.CharField(max_length=255)
@@ -116,4 +117,7 @@ class Car(models.Model):
     no_of_owner = models.CharField(max_length=255)
     is_featured = models.BooleanField(default=False)
     created_date = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return self.car_title
 
