7f280e7dec5c8a96b964da8adc07d5ae68d2d540
Sabbir Hossain <36231104+sabbirhossain540@users.noreply.github.com>
2022-01-01 22:02:58 +0600

    Update settings.py

--- carzone/settings.py
+++ carzone/settings.py
@@ -173,6 +173,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'bcadc555@gmail.com'
-EMAIL_HOST_PASSWORD = 965874321
+EMAIL_HOST_PASSWORD = your password
 EMAIL_USE_TLS = True
 
